853d3daa4f003deca4313c0902dea97c8d32c44e
Alex-NRCan <alexandre.roy@nrcan-rncan.gc.ca>
2026-09-10 16:39:14 -0400

    Updates

--- workers/fetch-esri-worker-script.worker.js
+++ workers/fetch-esri-worker-script.worker.js
@@ -1,4 +1,4 @@
-/*! Package:fetch-esri-worker-script: 2.3.0 - "2a46b5c4bd2b7645a358144596f9e564299625a0" - 2026-09-10T19:31:47.873Z */(()=>{"use strict";
+/*! Package:fetch-esri-worker-script: 2.3.0 - "6944666adcfe59f55446a448b74dc403bde11d8f" - 2026-09-10T20:35:21.432Z */(()=>{"use strict";
 /**
  * @license
  * Copyright 2019 Google LLC
